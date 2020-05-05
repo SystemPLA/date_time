ed983ab2c40eb638a4e62e94ee946ee6960ec791
SystemPLA <amalovatyj@yandex.ru>
2020-05-05 18:20:25 +0300

    start dialog button

--- app/src/main/java/ru/systempla/datetime/MainActivity.java
+++ app/src/main/java/ru/systempla/datetime/MainActivity.java
@@ -1,14 +1,31 @@
 package ru.systempla.datetime;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @OnClick(R.id.button_start_dialog)
+    void getDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.activity_dialog, null);
+        dialogBuilder.setView(dialogView);
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
